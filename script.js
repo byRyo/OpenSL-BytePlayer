f0290b73775e37925b4c87f51ef1c82869fb028e
Ryo <rrblx511@gmail.com>
2026-07-08 16:31:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const base = Process.getModuleByName("libsound.so").base;
 const SoundManager_playSoundFromBytes = new NativeFunction(base.add(0x3E318), 'pointer', ['pointer']);
 const SoundManager_payloadToSound = new NativeFunction(base.add(0x3DCF8), 'pointer', ['pointer', 'size_t']);
 
-const customPayload = [];
+const customPayload = [
+        // Erika loading :sob:
+];
 
 const size = customPayload.length;
 const ptr = Memory.alloc(size);
