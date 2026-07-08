92e89a24e2269fdffd9f1adba063f1beaf099385
Ryo <rrblx511@gmail.com>
2026-07-08 16:08:26 +0200

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,19 @@
+const base = Process.getModuleByName("libsound.so").base;
+
+const SoundManager_playSoundFromBytes = new NativeFunction(base.add(0x3E318), 'pointer', ['pointer']);
+const SoundManager_payloadToSound = new NativeFunction(base.add(0x3DCF8), 'pointer', ['pointer', 'size_t']);
+
+const customPayload = [];
+
+function playEmbedded() {
+    const size = customPayload.length;
+    const ptr = Memory.alloc(size);
+        
+    const buffer = new Uint8Array(customPayload).buffer;
+    ptr.writeByteArray(buffer);
+
+    const handle = SoundManager_payloadToSound(ptr, size);
+    const result = SoundManager_playSoundFromBytes(handle);
+    return result;
+}
+playEmbedded();
